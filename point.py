6ed11cde5bf23d6edc4c23eb936c86a4bc805b88
masud1901 <akmolmasud5@gmail.com>
2024-05-09 19:28:47 +0600

    fixed a small bug

--- point.py
+++ point.py
@@ -1,12 +1,9 @@
-from rectangle import Rectangle
-
-
 class Point:
     def __init__(self, x: int, y: int) -> None:
         self.x = x
         self.y = y
 
-    def falls_in_rectangle(self, rectangle: Rectangle):
+    def falls_in_rectangle(self, rectangle):
         if (
             rectangle.point1.x < self.x < rectangle.point2.x
             and rectangle.point1.y < self.y < rectangle.point2.y
